500d56f6a54898ace3db8087563028fd6c4519c1
Mike Schwörer <mailport@mikescher.de>
2014-06-07 15:45:21 +0200

    Added full parsing / data-export / data-import

--- demo.php
+++ demo.php
@@ -6,6 +6,7 @@ $v = new ExtendedGitGraph('Mikescher');
 
 //$v->authenticate('7e26c5f1621349c14a7d');
 
-$v->setToken('7b3f6443cdd4b2f92d75c4c8aa83cfda6c7ca3ce');
+//$v->setToken('7b3f6443cdd4b2f92d75c4c8aa83cfda6c7ca3ce');
+//$v->collect();
 
-$v->generate();
+$v->load();
--- extendedGitGraph.php
+++ extendedGitGraph.php
@@ -14,15 +14,17 @@ class ExtendedGitGraph {
 	const API_RATELIMIT = 'https://api.github.com/rate_limit';
 	const API_REPOSITORIESLIST = 'https://api.github.com/users/%s/repos?page=%d&per_page=100';
 
-	public $username;
-	public $token;
-	public $tokenHeader;
+	private $username;
+	private $token;
+	private $tokenHeader;
 
-	public $repositories;
-	public $commits;
+	private $repositories;
+	private $commits;
 
 	public function __construct($usr_name) {
 		$this->username = $usr_name;
+
+		set_time_limit(300); // 5min
 	}
 
 	public function authenticate($auth_key) {
@@ -40,19 +42,21 @@ class ExtendedGitGraph {
 		$this->tokenHeader = 'access_token=' . $token . '&token_type=bearer';
 	}
 
-	public function generate() {
+	public function collect() {
+		ob_implicit_flush(true);
+		ob_end_flush();
+
+		//--------------------------------
+
 		$this->listRepositories();
 
 		$this->listAllCommits();
 
-		//------------
+		$this->save();
 
-		echo nl2br(print_r($this->repositories, true));
-		echo '<hr>';
-		echo nl2br(print_r($this->commits, true));
-		echo '<hr>';
-		echo $this->getRemainingRequests() . ' Requests remaining';
-		echo '<hr>';
+		//--------------------------------
+
+		$this->output_flushed($this->getRemainingRequests() . ' Requests remaining');
 	}
 
 	private function listRepositories() {
@@ -66,6 +70,8 @@ class ExtendedGitGraph {
 		while (! empty($result)) {
 			foreach ($result as $result_repo) {
 				$repo_list[] = $this->parseRepoJSON($result_repo);
+
+				$this->output_flushed("Found Repo: " . $result_repo->{'full_name'});
 			}
 
 			//##########
@@ -79,8 +85,8 @@ class ExtendedGitGraph {
 	}
 
 	private function getJSON($url) {
-//		$options  = array('http' => array('user_agent'=> $_SERVER['HTTP_USER_AGENT']));
-		$options  = array('http' => array('user_agent'=> 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36'));
+		$options  = array('http' => array('user_agent'=> $_SERVER['HTTP_USER_AGENT']));
+//		$options  = array('http' => array('user_agent'=> 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36'));
 		$context  = stream_context_create($options);
 		$response = file_get_contents($url, false, $context);
 
@@ -93,26 +99,12 @@ class ExtendedGitGraph {
 		return $json->{'resources'}->{'core'}->{'remaining'};
 	}
 
-	private function parseRepoJSON($json) {
-		return
-			[
-				'id' => $json->{'id'},
-				'name' => $json->{'name'},
-				'full_name' => $json->{'full_name'},
-				'commits_url' => str_replace('{/sha}', '', $json->{'commits_url'}),
-			];
-	}
-
 	private function listAllCommits() {
 		$this->commits = array();
 
-//		foreach($this->repositories as $repo) {
-//			$this->listCommits($repo);
-//		}
-
-		$this->listCommits($this->repositories[0]);
-		$this->listCommits($this->repositories[1]);
-		$this->listCommits($this->repositories[2]);
+		foreach($this->repositories as $repo) {
+			$this->listCommits($repo);
+		}
 	}
 
 	private function listCommits($repo) {
@@ -128,6 +120,8 @@ class ExtendedGitGraph {
 				$commit_list[] = $this->parseCommitJSON($repo, $result_commit);
 			}
 
+			$this->output_flushed("Found 100 Commits from  " . $repo['full_name']);
+
 			//##########
 
 			$url = $repo['commits_url'] . '?per_page=100&page=' . ++$page . '&author=' .$this->username . '&' .$this->tokenHeader;
@@ -138,13 +132,64 @@ class ExtendedGitGraph {
 		$this->commits = array_merge($this->commits, $commit_list);
 	}
 
+	private function parseRepoJSON($json) {
+		return
+			[
+				'id' => $json->{'id'},
+				'name' => $json->{'name'},
+				'full_name' => $json->{'full_name'},
+				'owner' => $json->{'owner'}->{'login'},
+				'owner_id' => $json->{'owner'}->{'id'},
+				'owner_avatar-url' => $json->{'owner'}->{'avatar_url'},
+				'owner_gravatar-id' => $json->{'owner'}->{'gravatar_id'},
+				'url' => $json->{'html_url'},
+				'language' => $json->{'language'},
+				'url' => $json->{'html_url'},
+				'creation' => DateTime::createFromFormat(DateTime::ISO8601, $json->{'created_at'}),
+				'size' => $json->{'size'},
+				'default_branch' => $json->{'default_branch'},
+				'commits_url' => str_replace('{/sha}', '', $json->{'commits_url'}),
+			];
+	}
+
 	private function parseCommitJSON($repo, $json) {
 		return
 			[
+				'sha' => $json->{'sha'},
+				'author_name' => $json->{'commit'}->{'author'}->{'name'},
+				'author_mail' => $json->{'commit'}->{'author'}->{'email'},
+				'author_login' => $json->{'author'}->{'login'},
+				'author_id' => $json->{'author'}->{'id'},
 				'sha' => $json->{'sha'},
 				'message' => $json->{'commit'}->{'message'},
 				'repository' => $repo,
 				'date' => DateTime::createFromFormat(DateTime::ISO8601, $json->{'commit'}->{'author'}->{'date'}),
 			];
+	}
+
+	private function save() {
+		$this->output_flushed("Start saving data");
+
+		$save = serialize(
+			[
+				'repositories' => $this->repositories,
+				'commits' => $this->commits,
+			]);
+
+		file_put_contents('ext_git_graph.dat', $save);
+
+		$this->output_flushed('Finished saving data');
+	}
+
+	private function output_flushed($txt)
+	{
+		echo '[' . date('H:i.s') . '] ' . $txt . "<br>";
+	}
+
+	public function load() {
+		$data = unserialize(file_get_contents('ext_git_graph.dat'));
+
+		$this->repositories = $data['repositories'];
+		$this->commits = $data['commits'];
 	}
 } 
