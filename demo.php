e8e279706c34731fad3361701634d9791e271a22
Mike Schwörer <mailport@mikescher.de>
2014-10-14 22:00:30 +0200

    Added:     Ajax Refresh     Organizations     Foreign Repos     Better Output     Bugfixes

--- demo.php
+++ demo.php
@@ -4,12 +4,9 @@ include 'extendedGitGraph.php';
 
 $v = new ExtendedGitGraph('Mikescher');
 
-//$v->authenticate('???', '???', '???');
+$v->addSecondaryUsername("Sam-Development");
 
-//$v->setToken('???');
-//$v->collect();
-
-$v->loadData();
+$v->addSecondaryRepository("Anastron/ColorRunner");
 
 ?>
 
@@ -24,11 +21,70 @@ $v->loadData();
 		<script type="text/javascript" language="JavaScript">
 			<?php include 'script.js'; ?>
 		</script>
+
+		<script type="text/javascript" language="JavaScript">
+			function startAjaxRefresh()
+			{
+				$('#ajaxOutput').val("");
+
+				val = setInterval(
+					function()
+					{
+						jQuery.ajax({
+							url:    'ajaxStatus.php',
+							success: function(result)
+							{
+								$('#ajaxOutput').val(result);
+								$('#ajaxOutput').scrollTop($('#ajaxOutput')[0].scrollHeight);
+							},
+							async:   false
+						});
+					}, 500);
+
+				jQuery.ajax({
+					url:    'ajaxReload.php',
+					success: function(result)
+					{
+						clearInterval(val);
+
+						jQuery.ajax({
+							url:    'ajaxStatus.php',
+							success: function(result)
+							{
+								$('#ajaxOutput').val(result + '\r\n.');
+								$('#ajaxOutput').scrollTop($('#ajaxOutput')[0].scrollHeight);
+							},
+							async:   true
+						});
+					},
+					async:   true
+				});
+
+			}
+		</script>
+
 	</head>
 	<body>
+		<?php echo $v->loadFinishedContent(); ?>
+
+		<textarea style="width: 800px; height: 250px;" id="ajaxOutput" readonly="readonly"></textarea>
+
+		<br>
+
+		<a href="javascript:startAjaxRefresh()">[REFRESH]</a>
+
+
 		<?php
-			echo $v->generateAndSave();
-			//echo $v->loadFinished();
+
+		if ( isset($_GET['collect']) && $_GET['collect'] === 'true')
+		{
+			$v->setToken(file_get_contents('api_token.secret'));
+			$v->collect();
+
+			$v->generateAndSave();
+		}
+
 		?>
+
 	</body>
 </html>
