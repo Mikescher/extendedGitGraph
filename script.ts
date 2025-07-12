bbd7b0fa73520e81a156c90945dd0ed59bcf59d9
Mike Schwörer <pubgit@mikescher.com>
2025-07-12 18:32:25 +0200

    Output per-repo counts

--- script.ts
+++ script.ts
@@ -12,6 +12,7 @@ namespace ExtendedGitGraph2
 
         const masterTipHeader  = masterTip.getElementsByTagName('strong')[0];
         const masterTipContent = masterTip.getElementsByTagName('span')[0];
+        const masterTipExtra = masterTip.getElementsByTagName('div')[0];
 
         const rects = Array.from(document.getElementsByClassName("egg_rect")) as HTMLElement[];
 
@@ -29,11 +30,35 @@ namespace ExtendedGitGraph2
                 masterTipHeader.innerHTML = count + ' commits';
                 masterTipContent.innerHTML = ' on ' + date;
 
+                if (event.ctrlKey || event.metaKey || event.shiftKey || event.altKey) {
+
+                    masterTipExtra.style.display = 'grid';
+
+                    const extraAttr = target.getAttributeNames().sort().filter(p => p.startsWith('data-repo-')).map(p => target.getAttribute(p)!);
+
+                    let extraHTML = '';
+                    for (const attr of extraAttr) {
+                        try {
+                            let obj = JSON.parse(attr.replace(/'/g, '"'));
+                            extraHTML += `<span class="extra-attr">${obj.repo_name}:</span><strong>${obj.count}</strong>\n`;
+                        } catch (e) {
+                            console.error('Error parsing extra attribute:', attr, e);
+                        }
+                    }
+
+                    masterTipExtra.innerHTML = extraHTML;
+
+                } else {
+
+                    masterTipExtra.style.display = 'none';
+
+                }
+
                 masterTip.style.left = (window.pageXOffset + target.getBoundingClientRect().left - masterTip.getBoundingClientRect().width /2 - 3.5 + 9) + 'px';
                 masterTip.style.top  = (window.pageYOffset + target.getBoundingClientRect().top  - masterTip.getBoundingClientRect().height -10)         + 'px';
             });
 
-            rect.addEventListener("mouseleave", _ => masterTip.style.display = 'none');
+            //rect.addEventListener("mouseleave", _ => masterTip.style.display = 'none');
         }
     }
 }
