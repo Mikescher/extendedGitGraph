31d8da7e87d2ab4d0a5ca540ff7da446c5d793d6
Mike Schwörer <pubgit@mikescher.com>
2025-07-18 23:52:34 +0200

    Fix EGG tooltip css/script

--- script.ts
+++ script.ts
@@ -2,64 +2,66 @@ namespace ExtendedGitGraph2
 {
     export function initHover()
     {
-        const allsvgtips = Array.from(document.getElementsByClassName("svg-tip"));
-        if (allsvgtips.length == 0) return;
+        for (const container of document.querySelectorAll('.extGitGraphContainer')) {
 
-        const masterTip = allsvgtips[0] as HTMLElement;
+            const masterTip = container.querySelector('.svg-tip') as (HTMLElement|null);
+            if (masterTip === null) continue;
 
-        masterTip.style.opacity = '1';
-        masterTip.style.display = 'none';
+            masterTip.style.opacity = '1';
+            masterTip.style.display = 'none';
 
-        const masterTipHeader  = masterTip.getElementsByTagName('strong')[0];
-        const masterTipContent = masterTip.getElementsByTagName('span')[0];
-        const masterTipExtra = masterTip.getElementsByTagName('div')[0];
+            const masterTipHeader  = masterTip.querySelector('.caption')! as HTMLElement;
+            const masterTipContent = masterTip.querySelector('.date')!    as HTMLElement;
+            const masterTipExtra   = masterTip.querySelector('.extra')!   as HTMLElement;
 
-        const rects = Array.from(document.getElementsByClassName("egg_rect")) as HTMLElement[];
+            const rects = Array.from(container.querySelectorAll(".egg_rect")) as HTMLElement[];
 
-        for (let rect of rects)
-        {
-            rect.addEventListener("mouseenter", event =>
+            for (let rect of rects)
             {
-                const target = event.target as HTMLElement;
+                rect.addEventListener("mouseenter", event =>
+                {
+                    const target = event.target as HTMLElement;
 
-                let count = target.getAttribute('data-count');
-                let date  = target.getAttribute('data-date');
+                    let count = target.getAttribute('data-count');
+                    let date  = target.getAttribute('data-date');
 
-                masterTip.style.display = 'block';
+                    masterTip.style.display = 'block';
 
-                masterTipHeader.innerHTML = count + ' commits';
-                masterTipContent.innerHTML = ' on ' + date;
+                    masterTipHeader.innerHTML = count + ' commits';
+                    masterTipContent.innerHTML = ' on ' + date;
 
-                if (event.ctrlKey || event.metaKey || event.shiftKey || event.altKey) {
+                    if (event.ctrlKey || event.metaKey || event.shiftKey || event.altKey) {
 
-                    masterTipExtra.style.display = 'grid';
+                        masterTipExtra.style.display = 'grid';
 
-                    const extraAttr = target.getAttributeNames().sort().filter(p => p.startsWith('data-repo-')).map(p => target.getAttribute(p)!);
+                        const extraAttr = target.getAttributeNames().sort().filter(p => p.startsWith('data-repo-')).map(p => target.getAttribute(p)!);
 
-                    let extraHTML = '';
-                    for (const attr of extraAttr) {
-                        try {
-                            let obj = JSON.parse(attr.replace(/'/g, '"'));
-                            extraHTML += `<span class="extra-attr">${obj.repo_name}:</span><strong>${obj.count}</strong>\n`;
-                        } catch (e) {
-                            console.error('Error parsing extra attribute:', attr, e);
+                        let extraHTML = '';
+                        for (const attr of extraAttr) {
+                            try {
+                                let obj = JSON.parse(attr.replace(/'/g, '"'));
+                                extraHTML += `<span class="extra-attr">${obj.repo_name}:</span><strong>${obj.count}</strong>\n`;
+                            } catch (e) {
+                                console.error('Error parsing extra attribute:', attr, e);
+                            }
                         }
+
+                        masterTipExtra.innerHTML = extraHTML;
+
+                    } else {
+
+                        masterTipExtra.style.display = 'none';
+
                     }
 
-                    masterTipExtra.innerHTML = extraHTML;
+                    masterTip.style.left = (window.pageXOffset + target.getBoundingClientRect().left - masterTip.getBoundingClientRect().width /2 - 3.5 + 9) + 'px';
+                    masterTip.style.top  = (window.pageYOffset + target.getBoundingClientRect().top  - masterTip.getBoundingClientRect().height -10)         + 'px';
+                });
 
-                } else {
-
-                    masterTipExtra.style.display = 'none';
-
-                }
-
-                masterTip.style.left = (window.pageXOffset + target.getBoundingClientRect().left - masterTip.getBoundingClientRect().width /2 - 3.5 + 9) + 'px';
-                masterTip.style.top  = (window.pageYOffset + target.getBoundingClientRect().top  - masterTip.getBoundingClientRect().height -10)         + 'px';
-            });
-
-            //rect.addEventListener("mouseleave", _ => masterTip.style.display = 'none');
+                rect.addEventListener("mouseleave", _ => masterTip.style.display = 'none');
+            }
         }
+
     }
 }
 
